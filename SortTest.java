e1ea2d49d2fa8af34b20009760ff1664ff2a8975
杨子昕 <49299743+ZixinYZX@users.noreply.github.com>
2019-04-25 21:19:32 +0800

    Update SortTest.java

--- SortTest.java
+++ SortTest.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Random;
 
+
+//测试
 public class SortTest {
 	public static void main(String[] args) {
 		int[] arr1 = new int[100000000];
@@ -18,15 +20,22 @@ public class SortTest {
 		Arrays.sort(arr1);
 		
 		long endTime=System.currentTimeMillis();
-		System.out.println("java排序运行时间： "+(endTime-startTime)+"ms");
+		System.out.println("快速排序运行时间： "+(endTime-startTime)+"ms");
 		
-		startTime=System.currentTimeMillis(); 
 		
-		//InsertSort.sort(arr2);
+		long startTime=System.currentTimeMillis(); 
+		
+		InsertSort.sort(arr2);
+		
+		long endTime=System.currentTimeMillis();
+		System.out.println("插入排序运行时间： "+(endTime-startTime)+"ms");
+		
+		startTime=System.currentTimeMillis();
 		ShellSort.sort(arr2);
-		
 		endTime=System.currentTimeMillis();
-		System.out.println("排序运行时间： "+(endTime-startTime)+"ms");
+		System.out.println("希尔排序运行时间： "+(endTime-startTime)+"ms");
+		
+		
 		int j = 0;
 		while(j < arr1.length&&arr1[j] == arr2[j]){
 			j++;
@@ -38,3 +47,45 @@ public class SortTest {
 			System.out.print("Fail");
 	}
 }
+
+
+//希尔排序
+public class ShellSort {
+
+	static void sort(int[] arr) {
+		int gap = 1;
+		while (gap < arr.length/3) {
+			gap = gap * 3 + 1;
+		}
+
+		while (gap > 0) {
+			for (int i = gap; i < arr.length; i++) {
+				int tmp = arr[i];
+				int j = i - gap;
+				while (j >= 0 && arr[j] > tmp) {
+					arr[j + gap] = arr[j];
+					j -= gap;
+				}
+				arr[j + gap] = tmp;
+			}
+		//	gap = (int) Math.floor(gap / 3);
+			gap = (gap-1)/3; 
+		}
+	}	
+//插入排序
+public class InsertSort {
+	
+	static void sort(int[] a) {
+		for (int i = 1; i < a.length; i++) {
+			int temp = a[i]; // 记录要插入的数据
+			int j = i;
+			
+			while (j>0 && temp < a[j-1]) {
+				a[j] = a[j - 1];
+				j--;
+			}
+			if (i != j)
+				a[j] = temp;
+		}
+	}
+
